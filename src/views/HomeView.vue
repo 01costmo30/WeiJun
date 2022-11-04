778e4e72dd512201660a6744b17b0e648d84e30c
01costmo30 <01lu301998@gmail.com>
2022-11-04 17:32:01 +0800

    hehe

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,8 +5,8 @@ import TheWelcome from '../components/TheWelcome.vue'
 <template>
   <main>
     <!-- <TheWelcome /> -->
-    <img src="310115993_823633578826798_5542614640501985338_n.jpg" />
-    <img src="310138593_125517036962257_6990391018990110775_n.png" />
+    <img src="/310115993_823633578826798_5542614640501985338_n.jpg" />
+    <img src="/310138593_125517036962257_6990391018990110775_n.png" />
   </main>
 </template>
 <style scoped>
