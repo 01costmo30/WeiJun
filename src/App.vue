5914a0257541ddfe716e8a7538dff17eaa2656ff
京承科技公司機 <jcti@jingchengkejigongsijideMacBook-Air.local>
2022-11-07 01:13:44 +0800

    img config

--- src/App.vue
+++ src/App.vue
@@ -21,7 +21,7 @@ onMounted(() => {
   <div class="ui two column doubling grid">
     <div class="column">
 
-      <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+      <img alt="Vue logo" class="logo" src="img/3.png" width="125" height="125" />
 
       <div class="ui basic massive segment">
         <!-- <HelloWorld msg="You did it!" /> -->
@@ -37,9 +37,9 @@ onMounted(() => {
       </div>
     </div>
     <div id="scene" class="column">
-      <div data-depth="1" class="ui medium pink image circle"></div>
-      <div data-depth="0.6" class="ui big purple image circle"></div>
-      <img data-depth="0.2" src="https://i.ebayimg.com/images/g/emwAAOSwW5dgECZ4/s-l1600.png" />
+      <div data-depth="1" data-originX="0" class="ui medium pink image circle"></div>
+      <div data-depth="0.6" data-originX="0.5" class="ui big purple image circle"></div>
+      <img data-depth="0.2" data-originX="0" class="ui image" src="img/Photo1.png" />
     </div>
   </div>
 
@@ -72,9 +72,13 @@ div>.big.circle {
   margin: 6em 0 0 6em;
 }
 
-#scene img {
-  margin: -14em 0 0 -36em;
+#scene {
+  min-height: 100vh;
 }
+
+/* #scene img {
+  margin: 0em 0 0 -6em;
+} */
 
 .ui.medium.circle.image {
   height: 300px;
