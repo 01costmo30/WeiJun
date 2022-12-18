6a101aca14c108ac0dc1d882cddae9bf6e38ef6e
01costmo30 <01lu301998@gmail.com>
2022-12-19 00:27:30 +0800

    minor change

---
+++ src/components/ContactInfo.vue
@@ -0,0 +1,76 @@
+<script setup>
+import { onMounted } from 'vue';
+
+onMounted(()=> {
+  $('.ui.overlay.fullscreen.modal')
+    .modal({
+      inverted: true,
+      blurring: true,
+      onVisible: function () {
+        $('.column img.image')
+          .visibility({
+            context: '.scrolling.content',
+            type: 'image',
+            transition: 'fade in',
+            duration: 1000
+          });
+      }
+    }).modal('show');
+})
+</script>
+<template>
+
+  <div class="ui overlay fullscreen modal">
+    <i class="close icon"></i>
+    <div class="scrolling content">
+      <div class="ui three column doubling stackable vary padded grid">
+        <div class="column">
+          <img class="ui image" data-src="QR code/OfficalQRCode.png" />
+          <a href="https://lin.ee/4k4FgLt" target="_blank">
+            <h1 class="ui green center aligned basic header label">LINE</h1>
+          </a>
+        </div>
+        <div class="column">
+          <img class="ui image" data-src="QR code/qrCode.png" />
+          <a href="https://www.facebook.com/ilovesinminli/?ref=page_internal" target="_blank">
+            <h1 class="ui blue center aligned basic header label">FB粉專</h1>
+          </a>
+        </div>
+        <div class="column">
+          <img class="ui image" data-src="QR code/意見表qrCode.png" />
+          <a href="https://forms.gle/LJdoo7mNJuumt9C17" target="_blank">
+            <h1 class="ui violet center aligned basic header label">意見表單</h1>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<style scoped>
+.ui.overlay.fullscreen.modal,
+.ui.overlay.fullscreen.modal .content {
+  background: center / cover no-repeat url('@/assets/mesh-743.png');
+}
+
+.ui.ui.overlay.fullscreen.modal .scrolling.content {
+  height: 100vh;
+  height: calc(var(--vh, 1vh) * 100);
+  max-height: 100vh;
+  max-height: calc(var(--vh, 1vh) * 100);
+}
+
+.ui.modal .content .ui.grid {
+  min-height: 100%;
+}
+
+.ui.modal .content .column img.image {
+  filter: drop-shadow(0px 0px 8px white);
+}
+
+.ui.modal .content .header.label {
+  border-radius: 2em;
+  width: 100%;
+  margin-top: .5em;
+}
+
+</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,18 +1,81 @@
 <script setup>
+import { onMounted } from 'vue';
 import TopMenu from '../components/TopMenu.vue';
+onMounted(() => {
+  $('.ui.button[data-page]').on('click', function () {
+    var prev = $('.shape .side.active').data('page')
+    var next = $(this).data('page')
 
+    $('.ui.button[data-page]').removeClass('active')
+    $(this).addClass('active')
+
+    if (prev > next)
+      $('.shape').shape('set next side',
+        '.side[data-page=' + next + ']').shape('flip left')
+    else
+      $('.shape').shape('set next side',
+        '.side[data-page=' + next + ']').shape('flip right')
+
+  })
+})
 </script>
 <template>
   <TopMenu></TopMenu>
   <!-- <RouterLink to="/WeiJun">新民里大小事 蔡煒鈞</RouterLink> -->
-  <div class="about">
-    
+  <div class="about ui container">
+    <div class="ui two column stackable grid">
+      <div class="column">
+        <div class="ui basic segment">
+          <div class="ui basic compact active button" data-page="1">你是誰？</div>
+          <div class="ui basic compact button" data-page="2">經歷</div>
+          <div class="ui basic compact button" data-page="3">為什麼想出來選里長？</div>
+          <div class="ui basic compact button" data-page="4">你年輕沒經驗，怎麼做事？</div>
+        </div>
+        <div class="ui square shape">
+          <div class="sides">
+            <div class="side active" data-page="1">
+              <div class="content">
+                <div class="justify">
+                  我是從小在新民里長大的煒鈞，大學期間加入多種服務性社團，不只經驗累積，也學到許多溝通、協調的技巧。只要交代給我的工作，都會盡力做到最完美。
+                </div>
+              </div>
+            </div>
+            <div class="side" data-page="2">
+              <div class="content">
+                <div class="justify">
+                  元智大學學生會、TEDxYZU成員、旅行與閱讀基金會-行銷企劃、天河創思出版社-執行編輯、元智大學-教學助理
+                </div>
+              </div>
+            </div>
+            <div class="side" data-page="3">
+              <div class="content">
+                <div class="justify">
+                  這個社區經過時間的流逝，我長大了，它卻衰老了。<br />
+                  有「生活感」的社區，應該是充滿人的氣息，處處有人活動的痕跡，新民里的年齡結構其實很健康，那大家究竟都去了哪裡？答案很簡單，因為沒有好的空間或活動能讓居民聚集。既然發現了問題，也想要改變，那就自己參與，共同打造一個理想社區該有的模樣。
+                </div>
+              </div>
+            </div>
+            <div class="side" data-page="4">
+              <div class="content">
+                <div class="justify">
+                  我認為年輕的優勢是善於用新方法解決舊問題，我們有新工具和創意，並且充滿熱情與活力，願意努力，期許能為新民里帶來新氣象。
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped>
 #app:has(.ui.secondary.menu) {
- background: url('@/assets/mesh-743.png');
+  background: url('@/assets/mesh-743.png');
+}
+
+.shape .side .justify {
+  text-align: justify;
 }
 </style>
  
